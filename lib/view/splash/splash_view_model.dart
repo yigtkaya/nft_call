7a1bd0662d9feaa7a5629d0e5c5512f4abd16cdd
Hasan Yiğit Kaya <hasanyigit.kaya@bahcesehir.edu.tr>
2023-01-18 01:03:52 +0300

    temizlik

--- lib/view/splash/splash_view_model.dart
+++ lib/view/splash/splash_view_model.dart
@@ -1,9 +1,6 @@
 import 'dart:io';
-import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:nft_call/auth/auth.dart';
-
 import '../../core/base/view/base_view_model.dart';
-import '../../product/menu/menu_key.dart';
 
 class SplashViewModel extends BaseViewModel<SplashViewModel> {
   final String tag = (SplashViewModel).toString();
