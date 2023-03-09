fc16dd56df8371289159ae426c721ce1a3778920
Hasan Yiğit Kaya <hasanyigit.kaya@bahcesehir.edu.tr>
2023-03-09 13:48:51 +0300

    more fix

--- lib/core/components/kt_text_field.dart
+++ lib/core/components/kt_text_field.dart
@@ -45,6 +45,7 @@ class _KTTextFieldState extends State<KTTextField> {
               borderRadius: BorderRadius.circular(10),
             ),
             child: TextFormField(
+              cursorColor: Colors.grey,
               autovalidateMode: AutovalidateMode.onUserInteraction,
                 obscureText: widget.title == "Email" ? false : true,
                 autocorrect: false,
