7971aca0a2d220c3ca599bc41f139a8df148d120
Hasan Yiğit Kaya <hasanyigit.kaya@bahcesehir.edu.tr>
2023-03-10 07:56:50 +0300

    drawer view update

--- lib/view/drawer/drawer_view.dart
+++ lib/view/drawer/drawer_view.dart
@@ -14,6 +14,7 @@ import 'package:nft_call/view/drawer/drawer_model.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../../core/base/view/view_info.dart';
+import '../../core/constants/theme/color/gradient_colors.dart';
 import '../../product/menu/menu_key.dart';
 import '../../product/menu/screen_name.dart';
 
@@ -27,111 +28,144 @@ class DrawerView extends BaseView<DrawerView, DrawerViewModel> {
     // TODO: implement build
     return SafeArea(
       child: Scaffold(
-        backgroundColor: const Color(0xf5263848),
         body: Center(
-            child: Column(
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [
+                      ColorConstants
+                          .colorPlateList[7 % (ColorConstants.colorPlateList.length)]
+                          .startColor,
+                      ColorConstants
+                          .colorPlateList[7 % (ColorConstants.colorPlateList.length)]
+                          .endColor,
+                    ],
+                    begin: Alignment.bottomRight,
+                    end: Alignment.topCenter,
+                    stops: const [0.0, 1.2],
+                    tileMode: TileMode.clamp),
+              ),
+              child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Padding(
-                    padding: const EdgeInsets.all(25),
-                    child: ITIcon(
-                      iconName: AssetConstants.icons.cross,
-                      onPress: () => Get.back(),
-                    )),
-              ],
-            ),
-            const Center(
-              child: Image(
-                image: AssetImage("assets/images/logo_temp.png"),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.all(25),
+                      child: ITIcon(
+                        iconName: AssetConstants.icons.cross,
+                        onPress: () => Get.back(),
+                      )),
+                ],
               ),
-            ),
-            const VerticalSpace(
-              spaceAmount: 50,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    TextButton(
-                      onPressed: () => launchURL("https://www.twitter.com"),
-                      child: Row(
-                        children: [
-                          ITIcon(
-                            iconName: AssetConstants.icons.twitter,
-                            width: 26,
-                            height: 26,
-                          ),
-                          const HorizontalSpace(
-                            spaceAmount: 20,
-                          ),
-                          DTText(
-                              label: "Follow us on Twitter",
-                              color: Colors.white,
-                              style: context.regular20)
-                        ],
+              const Center(
+                child: Image(
+                  image: AssetImage("assets/images/logo_temp.png"),
+                ),
+              ),
+              const VerticalSpace(
+                spaceAmount: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextButton(
+                        onPressed: () => launchURL("https://www.twitter.com"),
+                        child: Row(
+                          children: [
+                            ITIcon(
+                              iconName: AssetConstants.icons.twitter,
+                              width: 26,
+                              height: 26,
+                            ),
+                            const HorizontalSpace(
+                              spaceAmount: 20,
+                            ),
+                            DTText(
+                                label: "Follow us on Twitter",
+                                color: Colors.white,
+                                style: context.regular20)
+                          ],
+                        ),
                       ),
-                    ),
-                    TextButton(
-                      onPressed: () => launchURL("mailto:yigitkaya@vivitate.com?subject=<subject>&body=<body>"),
-                      child: Row(
-                        children: [
-                          ITIcon(
-                            iconName: AssetConstants.icons.gmail,
-                            width: 26,
-                            height: 26,
-                          ),
-                          const HorizontalSpace(
-                            spaceAmount: 20,
-                          ),
-                          DTText(
-                              label: "Contact us",
-                              color: Colors.white,
-                              style: context.regular20)
-                        ],
+                      TextButton(
+                        onPressed: () => launchURL(
+                            "mailto:yigitkaya@vivitate.com?subject=<subject>&body=<body>"),
+                        child: Row(
+                          children: [
+                            ITIcon(
+                              iconName: AssetConstants.icons.gmail,
+                              width: 26,
+                              height: 26,
+                            ),
+                            const HorizontalSpace(
+                              spaceAmount: 20,
+                            ),
+                            DTText(
+                                label: "Contact us",
+                                color: Colors.white,
+                                style: context.regular20)
+                          ],
+                        ),
                       ),
-                    ),
-
-                  ],
-                )
-              ],
-            ),
-            Expanded(
-              child: Align(
-                alignment: Alignment.center,
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 10.0),
-                  child: TextButton(
-                      onPressed: () => viewModel.signOut(),
-                      child:
-                      DTText(
-                          label: "Sign out",
-                          color: Colors.white,
-                          style: context.regular20)
-
+                      TextButton(
+                        onPressed: () => launchURL(
+                            "mailto:yigitkaya@vivitate.com?subject=Collection Apply&body=<body>"),
+                        child: Row(
+                          children: [
+                            ITIcon(
+                              iconName: AssetConstants.icons.gmail,
+                              width: 26,
+                              height: 26,
+                            ),
+                            const HorizontalSpace(
+                              spaceAmount: 20,
+                            ),
+                            DTText(
+                                label: "To add collection",
+                                color: Colors.white,
+                                style: context.regular20)
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: TextButton(
+                        onPressed: () => viewModel.signOut(),
+                        child: DTText(
+                            label: "Sign out",
+                            color: Colors.white,
+                            style: context.regular20)),
                   ),
                 ),
               ),
-            ),
-            Expanded(
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 10.0),
-                  child: DTText(
-                      label: "1.0.0",
-                      color: Colors.white,
-                      style: context.regular12),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: DTText(
+                        label: "1.0.0",
+                        color: Colors.white,
+                        style: context.regular12),
+                  ),
                 ),
               ),
-            ),
           ],
-        )),
+        ),
+            )),
       ),
     );
   }
